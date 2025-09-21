3aa8ab35d7cb85bfb046aff3add655d69922d76b
Naman rai <namanrai309@gmail.com>
2025-09-22 03:34:17 +0530

    fix footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,7 @@ function page() {
       <SlidelLayout />
       <Projects />
 
-      <div className="relative w-screen overflow-x-hidden">
+      <div className="relative w-screen">
         <WhyDubai />
         <FooterSection />
       </div>
